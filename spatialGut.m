d2d2525a448a22f6ad7b0bf0c055eeca95216093
shjchan <joshua.chan@colostate.edu>
2019-12-12 09:11:27 -0700

    Add killSpecies into the main function

--- spatialGut.m
+++ spatialGut.m
@@ -761,3 +761,54 @@ switch paramName
 end
 end
 
+function [modelK] = killSpecies(model, species)
+% [modelK] = killSpecies(model, species)
+% return a model shutting off all reactions of the given species
+%
+% INPUTS: 
+%    model:          COBRA community model with the additional fields `indCom` and `infoCom`
+%    species:        abbreviations or Ids of species to shut down
+%
+% OUTPUT:
+%    modelK:         updated model with the designated species inactive
+%
+% EXAMPLE:
+%    Create a model with only the first species active, all other being inactivated.
+%    modelK = killSpecies(model, setdiff(1:numel(model.infoCom.spAbbr), 1));
+
+if ischar(species)
+    species = {species};
+end
+
+if iscell(species)
+    del = false(numel(species), 1);
+    species2 = zeros(numel(species),1);
+    for j = 1:numel(species)
+        f = find(strcmp(species{j}, model.sps));
+        if ~isempty(f)
+            species2(j) = f;
+        elseif ~strcmp(species{j}, 'com') 
+            del(j) = true;
+        end
+        %else if species{j} = 'com', not allowing community exchange, for checking loops
+    end
+    species2(del) = [];
+elseif (islogical(species) || all(species == 0 | species == 1)) && numel(species) == numel(model.indCom.spBm)
+    species2 = find(species);
+elseif isnumeric(species)
+    species2 = species(species <= numel(model.indCom.spBm));
+else
+    warning('Incorrect input species.')
+    modelK = [];
+    return
+end
+
+
+modelK = model;
+for j = 1:numel(species2)
+    modelK.lb(modelK.indCom.rxnSps == species2(j)) = 0;
+    modelK.ub(modelK.indCom.rxnSps == species2(j)) = 0;
+end
+end
+
+
